9e1221d6e4c87e25ce122841a071886680c9bf48
Bekanzar007 <saitovbeknazar6@gmail.com>
2022-04-08 07:09:18 +0600

    adding yml file

--- app.py
+++ app.py
@@ -1,31 +1,17 @@
-from flask import Flask
+from flask import Flask, redirect, url_for,render_template
+
+
+import webbrowser
+
+
 
 app = Flask(__name__)
 
-def generate_html(message):
-    version_number = '0001'
-    html = """
-        <html>
-        <body>
-            <div style='text-align:center;font-size:80px;'>
-                <image height="340" width="1200" src="https://user-images.githubusercontent.com/194400/41597205-a57442ea-73c4-11e8-9591-61f5c83c7e66.png">
-                <h1>Adding some parts</h1>
-                <br> {0}
-                <p>Version Number: {1}</p>
-                <br>
-            </div>
-        </body>
-        </html>""".format(message,version_number)
-    return html
-
-def greet():
-    greeting = 'Welcome to CI/CD'
-    return greeting
 
 @app.route('/')
 def hello_world():
-    html = generate_html(greet())
-    return html
+    # return render_template("index.html")
+    return webbrowser.open('index.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
